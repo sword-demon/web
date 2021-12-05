b0d4a4d64223e68fd04bd547e6f259cffa5d0482
wxvirus <1418667580@qq.com>
2021-12-05 08:53:06 +0800

    es6学习迭代器

--- vue3/hd.js
+++ vue3/hd.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
         return {
             lessions: window.lessions,
             name: '根组件',
+            html: `<div style="color: red">无解 的game</div>`,
         }
     },
 })
@@ -19,3 +20,11 @@ app.component('wx', {
 // 返回根组件的实例
 const vm = app.mount('#app')
 // console.log(vm.name)
+
+// 根组件的实例
+console.log(vm.name)
+console.log(vm.$data.name)
+
+setTimeout(() => {
+    vm.$data.name = '无解的额游戏'
+}, 3000)
